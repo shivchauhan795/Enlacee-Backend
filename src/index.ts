4b4645760493a6ca05b8f3a2b59e48f58384e3fa
Shiv Chauhan <shivchauhan795@gmail.com>
2025-01-04 17:07:39 +0530

    basic backend created

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, TypeScript with Express!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
